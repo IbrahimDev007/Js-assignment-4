3f55b4fdbd6fd19e95dcb11b454b20d64145e854
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-07 16:21:38 +0600

    3rd Problem done

--- assignment-4.js
+++ assignment-4.js
@@ -19,3 +19,19 @@ function evenOdd(str) {
     /*This function checks the type of str using the typeof operator. If str is not a string, the function returns "It's not a string". If str is a string, the function calculates the length of the string using the length property, checks if the length is even or odd, and returns "Even" or "Odd" as specified in the problem statement.*/
     return "Odd";
 }
+// 3rd problem
+
+function isLGSeven(num) {
+    if (typeof num !== "number") {
+      return "It's not a number";
+    }
+  
+    let difference = num - 7;
+    if (difference < 7) {
+      return difference;
+    } else {
+      return 2 * difference;
+    }
+    /* This function checks the type of num using the typeof operator. If num is not a number, the function returns "It's not a number". If num is a number, the function calculates the absolute difference between num and 7 . If the difference is less than 7, the function returns the difference. If the difference is greater than or equal to 7, the function returns double of the difference.*/
+  }
+ 

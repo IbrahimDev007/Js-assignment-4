7c684e67144df54b7c59b2335a192f81782b1d7f
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-07 16:14:38 +0600

    2nd problem done

--- assignment-4.js
+++ assignment-4.js
@@ -7,3 +7,15 @@ function mindGame(num) {
 
     /* This function checks the type of num using the typeof operator. If num is not a number, the function returns "It's not a number". If num is a number, the function performs the calculation as specified and returns the result.*/
 }
+// 2nd problem
+function evenOdd(str) {
+    if (typeof str !== "string") {
+        return "It's not a string";
+    }
+
+    if (str.length % 2 === 0) {
+        return "Even";
+    }
+    /*This function checks the type of str using the typeof operator. If str is not a string, the function returns "It's not a string". If str is a string, the function calculates the length of the string using the length property, checks if the length is even or odd, and returns "Even" or "Odd" as specified in the problem statement.*/
+    return "Odd";
+}

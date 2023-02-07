2e0b17d7fd9ffd47ff6a5b230a18b2a7daca3818
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-07 16:28:08 +0600

    4th problem done

--- assignment-4.js
+++ assignment-4.js
@@ -34,4 +34,25 @@ function isLGSeven(num) {
     }
     /* This function checks the type of num using the typeof operator. If num is not a number, the function returns "It's not a number". If num is a number, the function calculates the absolute difference between num and 7 . If the difference is less than 7, the function returns the difference. If the difference is greater than or equal to 7, the function returns double of the difference.*/
   }
- 
+ //4th Problem
+
+ function findingBadData(arr) {
+    if (typeof arr !== "object" || !Array.isArray(arr)) {
+      return "It's not an array";
+    }
+  
+    let badDataCount = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== "number") {
+        return "Array contains non-numeric data";
+      }
+      if (arr[i] < 0) {
+        badDataCount++;
+      }
+    }
+
+    /*This function checks the type of arr using the typeof operator and the Array.isArray method. If arr is not an array, the function returns "It's not an array". If arr is an array, the function initializes a variable badDataCount to 0, which will be used to keep track of the number of negative numbers (bad data) in the array. The function then loops through the elements of the array using a for loop. For each element, the function checks if it is a number using the typeof operator. If it's not a number, the function returns "Array contains non-numeric data". If it's a number, the function checks if it is less than 0. If it is, the function increments the badDataCount variable. After the loop, the function returns the final count of bad data.*/
+  
+    return badDataCount;
+  }
+  
